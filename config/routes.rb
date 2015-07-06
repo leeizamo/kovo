ed470fc545b882b949a758140ae360b35c1aa4ca
Amour <leeizamo@gmail.com>
2015-07-06 00:53:22 -0400

    sass customization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root  'pages#home'
   get "about" => "pages#about" #creates 'about' path
+  get "projects" => "pages#projects" #creates 'projects' path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
